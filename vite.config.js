9420e14c527a7e7fcffd0d195ca75766dd710ffd
Davide9728 <davidemakenice@gmail.com>
2025-08-17 20:21:06 +0200

    Rimuovi file ignorati dal repository

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/fantacalcio-sito/',         // <-- es. '/fantacalcio-sito/'
-  build: { outDir: 'docs' }    // builda nella cartella 'docs'
-})
